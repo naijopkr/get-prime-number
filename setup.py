54a8147634c0da2991866c6f6c9713bef162e1fa
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-03-26 10:52:44 -0500

    updates setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name = 'get-prime-number',
-    packages = ['get-prime-number'],
     version = '0.1',
     description = 'A prime number generator',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author = 'Ariel Barcellos',
     author_email = 'contact@arielbarcellos.com',
     url = 'https://github.com/naijopkr/get-prime-number',
-    download_url = 'https://github.com/naijopkr/get-prime-number/tarball/0.1',
-    keywords = ['math', 'prime number'],
+    packages = find_packages(),
     classifiers = [
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"
