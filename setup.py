bddf7ec55812523b73d053db704402e8dee10444
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-03-26 09:16:28 -0500

    creates config files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'get-prime-number',
+    packages = ['get-prime-number'],
+    version = '0.1',
+    description = 'A prime number generator',
+    author = 'Ariel Barcellos',
+    author_email = 'contact@arielbarcellos.com',
+    url = 'https://github.com/naijopkr/get-prime-number',
+    download_url = 'https://github.com/naijopkr/get-prime-number/tarball/0.1',
+    keywords = ['math', 'prime number'],
+    classifiers = [],
+)
